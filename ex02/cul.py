c58b32987d7bf0a307c5d390af25d7b67944bfdc
C0B21180 <c0b211809b@edu.teu.ac.jp>
2022-06-21 15:31:11 +0900

    01

--- ex02/cul.py
+++ ex02/cul.py
@@ -1,43 +1,47 @@
+from ast import Num
 import tkinter as tk
 import tkinter.messagebox as tkm
+from turtle import right, width
  
 bg = tk.Tk()
 
-bg.geometry('300x500')
+bg.geometry('300x600')
 
 bg.title('calculater')
 
-def buttons(num):
-    a = tk.Button(bg,text=f"{num}",font=("Times New Roman",30),
-                width=4,height=2,command=lambda:coma(num))
-    return a
-
-def coma(n):
-    tkm.showinfo("",f"{n}のボタンがクリックされました")
-
-
-button1 = buttons(1)
-button2 = buttons(2)
-button3 = buttons(3)
-button4 = buttons(4)
-button5 = buttons(5)
-button6 = buttons(6)
-button7 = buttons(7)
-button8 = buttons(8)
-button9 = buttons(9)
-button0 = buttons(0)
+def click(event):
+    btn = event.widget
+    num = btn["text"]
+    if num == "=":
+        eqn = entry.get()
+        res = eval(eqn)
+        entry.delete(0,tk.END)
+        entry.insert(tk.END,res)
+    else:
+        #tkm.showinfo("",f"{n}のボタンがクリックされました")
+        entry.insert(tk.END,num)
 
 
-button1.grid(row=3,column=3)
-button2.grid(row=3,column=2)
-button3.grid(row=3,column=1)
-button4.grid(row=2,column=3)
-button5.grid(row=2,column=2)
-button6.grid(row=2,column=1)
-button7.grid(row=1,column=3)
-button8.grid(row=1,column=2)
-button9.grid(row=1,column=1)
-button0.grid(row=4,column=1,)
+entry = tk.Entry(bg,justify="right",width=10,font=("Times New Roman", 40))
+entry.grid(row=0,column=0,columnspan=3)
+
+
+r,c = 1,0
+for i , num in enumerate([9,8,7,6,5,4,3,2,1,0,"+","="]):
+    btn = tk.Button(bg,text=f"{num}",
+                    width=4,
+                    height=2,
+                    font=("Times New Roman",30)
+                )
+    btn.bind("<1>",click)
+    btn.grid(row=r,column=c)
+
+    c +=1
+    if (i+1)%3 == 0:
+        r +=1
+        c = 0
+
+
 
 
 bg.mainloop()

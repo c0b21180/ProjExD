cfc620c5ff90fd89cfb5fb4254d5281ed4ef1844
C0B21180 <c0b211809b@edu.teu.ac.jp>
2022-07-05 15:28:00 +0900

    練習7

--- ex04/pg.py
+++ ex04/pg.py
@@ -1,4 +1,4 @@
-from turtle import filling
+import random
 import pygame as pg
 import sys
 
@@ -16,6 +16,18 @@ def main():
     kkimg_rct = kkimg_sfc.get_rect()
     kkimg_rct.center = 900,400
 
+    # 爆弾
+    bmimg_sfc = pg.Surface((20,20))
+    bmimg_sfc.set_colorkey((0,0,0))
+    bmimg_sfc.set_colorkey((0,0,0))
+    pg.draw.circle(bmimg_sfc,(255,0,0),(10,10),10)
+    bmimg_rct = bmimg_sfc.get_rect()
+    bmimg_rct.centerx = random.randint(0,screen_rct.width)
+    bmimg_rct.centery = random.randint(0, screen_rct.height)
+    vx,vy = +1 , +1
+    
+
+
     while True:
         screen_sfc.blit(bgimg_sfc,bgimg_rct)
 
@@ -23,16 +35,33 @@ def main():
             if event.type == pg.QUIT: return
 
         key_states = pg.key.get_pressed()
-        if key_states[pg.K_UP] == True: kkimg_rct.centery -= 1
-        if key_states[pg.K_DOWN] == True: kkimg_rct.centery +=1
-        if key_states[pg.K_LEFT] == True: kkimg_rct.centerx -=1
-        if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx +=1
+        if key_states[pg.K_UP]      == True: kkimg_rct.centery -= 1
+        if key_states[pg.K_DOWN]    == True: kkimg_rct.centery +=1
+        if key_states[pg.K_LEFT]    == True: kkimg_rct.centerx -=1
+        if key_states[pg.K_RIGHT]   == True: kkimg_rct.centerx +=1
+        if check_bound(kkimg_rct,screen_rct) != (1,1):
+            if key_states[pg.K_UP]      == True: kkimg_rct.centery += 1
+            if key_states[pg.K_DOWN]    == True: kkimg_rct.centery -=1
+            if key_states[pg.K_LEFT]    == True: kkimg_rct.centerx +=1
+            if key_states[pg.K_RIGHT]   == True: kkimg_rct.centerx -=1
         screen_sfc.blit(kkimg_sfc,kkimg_rct)
+
+        bmimg_rct.move_ip(vx,vy)
+
+        screen_sfc.blit(bmimg_sfc,bmimg_rct)
+
+        yoko, tate = check_bound(bmimg_rct,screen_rct)
+        vx*=yoko
+        vy*=tate
 
         pg.display.update()
         clock.tick(1000)
 
-
+def check_bound(rct, scr_rct):
+    yoko , tate = +1 , +1
+    if rct.left < scr_rct.left or scr_rct.right < rct.right : yoko = -1
+    if rct.top < scr_rct.top or scr_rct.bottom < rct.bottom: tate = -1
+    return yoko , tate
 
 if __name__ == "__main__":
     pg.init()

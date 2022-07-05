ed29a8d4e307d04f2e029b7c9aecfca21c17842a
C0B21180 <c0b211809b@edu.teu.ac.jp>
2022-07-05 14:23:49 +0900

    練習３

---
+++ ex04/pg.py
@@ -0,0 +1,38 @@
+from turtle import filling
+import pygame as pg
+import sys
+
+def main():
+    clock = pg.time.Clock()
+    pg.display.set_caption("逃げろ！")
+    screen_sfc = pg.display.set_mode((1600,900))
+    screen_rct = screen_sfc.get_rect()
+    bgimg_sfc = pg.image.load("ex04/fig/6.jpg")
+    bgimg_rct = bgimg_sfc.get_rect()
+    screen_sfc.blit(bgimg_sfc,bgimg_rct)
+
+    kkimg_sfc = pg.image.load("ex04/fig/6.png")
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc,0,2.0)
+    kkimg_rct = kkimg_sfc.get_rect()
+    kkimg_rct.center = 900,400
+
+    while True:
+        screen_sfc.blit(bgimg_sfc,bgimg_rct)
+        screen_sfc.blit(kkimg_sfc,kkimg_rct)
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+    
+    
+    
+    
+        pg.display.update()
+        clock.tick(1000)
+
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit
+    sys.exit
